82c32ea1e86e6cd40e3fcbcb3f7d38bcc23a710e
BVBAccelm <brian@accelm.com>
2019-03-01 23:38:42 -0500

    expand toolbar functionality to allow virtually any customization

--- src/widgets/Toolbar.php
+++ src/widgets/Toolbar.php
@@ -2,6 +2,7 @@
 namespace bvb\admin\widgets;
 
 use yii\base\Widget;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use Yii;
 
@@ -18,10 +19,38 @@ class Toolbar extends Widget
     public $title;
 
     /**
+     * Configuration options for the tag wrapper for the title. If set to false or null will use title property raw
+     * @var array
+     */
+    public $titleTag = [
+        'tag' => 'h1',
+        'options' => [
+            'class' => 'h2'
+        ]
+    ];
+
+    /**
      * Buttons to be rendered in the right side of the toolbar
      * @var string
      */
     public $buttons;
+
+    /**
+     * Configuration options for the title. If set to false or null will use title property raw
+     * @var array
+     */
+    public $buttonContainer = [
+        'tag' => 'div',
+        'options' => [
+            'class' => 'btn-toolbar mb-2 mb-md-0'
+        ]
+    ];
+
+    /**
+     * Template used to adjust layout of elements
+     * @var string
+     */
+    public $template = '{title}{buttons}';
 
     /**
      * 
@@ -29,17 +58,26 @@ class Toolbar extends Widget
      */
     public function run()
     {
+        // --- Hold the parts in an array
+        $parts = [];
+
         // --- If there is no title set default to the title of the view
-        if(empty($this->title)){
-            $this->title = $this->getView()->title;
+        $parts['{title}'] = ($this->title === null) ? $this->getView()->title : $this->title;
+        if(!empty($this->titleTag) && !empty($parts['{title}'])){
+            $tag = ArrayHelper::remove($this->titleTag, 'tag');
+            $parts['{title}'] = Html::tag($tag, $parts['{title}'], $this->titleTag['options']);
         }
-        $title = Html::encode($this->title);
+
+        $parts['{buttons}'] = $this->buttons;
+        if(!empty($this->buttonContainer) && !empty($parts['{buttons}'])){
+            $tag = ArrayHelper::remove($this->buttonContainer, 'tag');
+            $parts['{buttons}'] = Html::tag($tag, $parts['{buttons}'], $this->buttonContainer['options']);
+        }
+
+        $content = strtr($this->template, $parts);
         $html = <<<HTML
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-    <h1 class="h2">{$title}</h1>
-    <div class="btn-toolbar mb-2 mb-md-0">
-        {$this->buttons}
-    </div>
+    $content
 </div>
 HTML;
         echo $html;
