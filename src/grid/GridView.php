8b0ff65d3651066ee68b00a17cabf836c0451684
BVBAccelm <brian@accelm.com>
2019-05-16 23:37:09 -0400

    add gridview with per-page results

---
+++ src/grid/GridView.php
@@ -0,0 +1,47 @@
+<?php
+namespace bvb\admin\grid;
+
+use yii\helpers\Html;
+
+class GridView extends \yii\grid\GridView
+{
+    /**
+     * @inheritdoc
+     */
+    public $filterSelector = '.filter-field';
+
+    /**
+     * @inheritdoc
+     */
+    public function renderSummary()
+    {
+        $summary = parent::renderSummary();
+        if(!empty($summary) && !empty($this->filterModel)){ // --- Also make sure the filter model is set because without it we can't render the dropdown
+            $summary = strip_tags($summary, '<b>');
+            $summary .=  ' Items per page: '.$this->getItemsPerPageDropown();
+            $summary = '<div class="summary form-inline">'.$summary.'</div>';
+        }
+        return $summary;
+    }
+
+    /**
+     * @param \yii\base\Model
+     * @return string
+     */
+    private function getItemsPerPageDropown()
+    {
+        return  Html::activeDropdownList(
+            $this->filterModel,
+            'per_page',
+            [
+                20 => 20,
+                50 => 50,
+                100 => 100,
+                1000 => 1000
+            ],
+            [
+                'class' => 'filter-field form-control'
+            ]
+        );
+    }
+}
