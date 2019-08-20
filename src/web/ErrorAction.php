45d8563d83efa0de3e40780b01f1c6d3993c8ebe
BVBAccelm <brian@accelm.com>
2019-08-19 23:02:14 -0400

    improve comment

--- src/web/ErrorAction.php
+++ src/web/ErrorAction.php
@@ -5,9 +5,8 @@ namespace bvb\admin\web;
 use Yii;
 
 /**
- * Override the default implementation to remove the layout and View component elements
- * for the form and the toolbar. Usually in apps in 'SiteController' there is a route set to
- * 'error' using \yii\web\ErrorAction. Replace that with this to use it. 
+ * Override the default implementation to remove the layout and View component 
+ * elements for the form and the toolbar. 
  */
 class ErrorAction extends \yii\web\ErrorAction
 {
