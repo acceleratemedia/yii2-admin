af530336ac3bfe35b013f4d8b8486ca943612807
BVBAccelm <brian@accelm.com>
2019-09-03 21:47:47 -0400

    allow linktextprefix to be passed in

--- src/widgets/NameSlugDisplay.php
+++ src/widgets/NameSlugDisplay.php
@@ -2,8 +2,10 @@
 
 namespace bvb\admin\widgets;
 
+use Yii;
 use yii\bootstrap4\Widget;
 use yii\helpers\Html;
+use yii\helpers\Inflector;
 
 /**
  * Displays a name and slug field together where the slug display
@@ -18,9 +20,16 @@ class NameSlugDisplay extends Widget
     public $model;
 
     /**
-     * @var \yii\bootstrap4\ActiveForm
+     * @var \yii\widgets\ActiveForm
      */
     public $form;
+
+    /**
+     * The text to be shown before the slug in the link display. Defaults to 
+     * current host info
+     * @var string
+     */
+    public $linkTextPrefix;
 
     /**
      * Overrides the default behavior to implement some fields that are common to all content
@@ -28,8 +37,12 @@ class NameSlugDisplay extends Widget
      */
     public function run()
     {
+        if(empty($this->linkTextPrefix)){
+            $this->linkTextPrefix = Yii::$app->request->getHostInfo().'/'.Inflector::camel2id($this->model->formName());
+        }
         return $this->render('name_slug_display', [
             'form' => $this->form,
+            'linkTextPrefix' => empty($this->linkTextPrefix) ?  : $this->linkTextPrefix,
             'model' => $this->model
         ]);
     }
