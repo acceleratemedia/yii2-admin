702cc70e9b477f971aca1e4921e9cd28e0654bea
BVBAccelm <brian@accelm.com>
2019-09-07 20:50:02 -0400

    set up container definitons used for admin styling

---
+++ src/config/container.php
@@ -0,0 +1,37 @@
+<?php
+
+// --- Holds site-wide common configuration for certain classes
+return [
+    'container' => [
+        'definitions' => [
+            // --- Special format for forms
+            \kartik\form\ActiveField::class => [
+                'hintType' => \kartik\form\ActiveField::HINT_SPECIAL,
+                'hintSettings' => [
+                    'onLabelHover' => false,
+                    'onLabelClick' => true,
+                ]
+            ],
+            // --- Default desired configuration for Select2 widget
+            \kartik\select2\Select2::class => [
+                'options' => [
+                    'placeholder' => 'Select one or more..',
+                    'encode' => false,
+                    'multiple' => true
+                ],
+                'pluginOptions' => [
+                    'allowClear' => true,
+                    'tags' => true,
+                    'closeOnSelect' => false
+                ]
+            ],
+            \kartik\datetime\DateTimePicker::class => [
+                'pluginOptions' => [
+                    'autoclose' => true,
+                    'todayBtn' => true,
+                    'format' => 'yyyy-mm-dd hh:mm:ss'
+                ]
+            ],
+        ]
+    ]
+];
--- src/web/View.php
+++ src/web/View.php
@@ -3,6 +3,7 @@
 namespace bvb\admin\web;
 
 use bvb\admin\assets\AppAsset;
+use Yii;
 
 /**
  * View is an extension of Yii's web View component adding properties that
@@ -11,44 +12,60 @@ use bvb\admin\assets\AppAsset;
 class View extends \yii\web\View
 {
     /**
-     * In this theme the default layout consists of a the top nav bar, side navigation, and a 'page' 
-     * area that in many cases will require a form. By providing the form as a property on the view
-     * component, we can access it in the layout as well as all rendered views. This provides the
-     * convenience of automatically wrapping the view in a form and being able to configure it
-     * in actions along with other layout components like the toolbar
+     * In this theme the default layout consists of a the top nav bar, side 
+     * navigation, and a 'page' area that in many cases will require a form.
+     * By providing the form as a property on the view component, we can access
+     * it in the layout as well as all rendered views. This provides the 
+     * convenience of automatically wrapping the view in a form and being able 
+     * to configure it in actions along with other layout components like the toolbar
      * @var \kartik\form\ActiveForm
      */
     public $form;
 
     /**
-     * The 'page' area of the default layout has the option of a toolbar being at the top. By providing
-     * this as a variable on the view component we can configure it along with the form across views
-     * as well as in actions.
+     * The 'page' area of the default layout has the option of a toolbar being
+     * at the top. By providing this as a variable on the view component we can
+     * configure it along with the form across views as well as in actions.
      * @var \kartik\form\ActiveForm
      */
     public $toolbar;
 
     /**
-     * The name of the asset bundle which will be registered in the layout file. By default it uses the
-     * asset bundle packaged with this admin theme. If you need additional CSS files to be rendered
-     * you may create your own AssetBundle class and the 'depends' property you can include the
+     * The name of the asset bundle which will be registered in the layout file.
+     * By default it uses the asset bundle packaged with this admin theme. If 
+     * you need additional CSS files to be rendered you may create your own 
+     * AssetBundle class and the 'depends' property you can include the
      * bvb\admin\asset\AppAsset bundle to keep loading this
      * @var string
      */
     public $assetBundle = AppAsset::class;
 
     /**
-     * Configuration for items to go in the side navigation. The following format can be used:
+     * Configuration for items to go in the side navigation. The format is the
+     * same as when using \yii\widgets\Menu with an additional 'weight' property
+     * for ordering the items and 'iconClass' for the font awesome icon to be 
+     * rendered if desired:
      *
      * ```php
      *   [
      *       'iconClass' => 'fas fa-newspaper',
      *       'label' => 'Articles',
      *       'url' => ['/article/index']
+     *       'weight' => 200,
+     *       'title' => 'Articles'
      *   ],
      * ```
      */
     public $sideNavItems = [];
+
+    /**
+     * Implement common container definitions desired for the admin setup
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        Yii::configure(Yii::$app, include(__DIR__.'/../config/container.php'));
+    }
 
     /**
      * Adds an item to the sidenav
